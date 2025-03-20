12e48669d35763a7983ebe392ea3e69106ab4ea0
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-20 13:12:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
-// Set the current date
-function setCurrentDate() {
-  const dateElement = document.getElementById("date");
-  const today = new Date();
-  const formattedDate = today.toDateString();
-  dateElement.textContent = formattedDate;
-}
-//The Event Listeners
-form.addEventListener("submit", getData);
+ubmit", getData);
 locBtn.addEventListener("click", getLocationData);
 window.addEventListener("load", setCurrentDate);
