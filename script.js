4f257b3d9ba2e2ae164da3b803d797ebc2c24416
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-20 13:12:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-ain.temp - 273.15).toFixed(1); // Convert Kelvin to Celsius
-dy.classList.remove("cold-background", "hot-background");
-
-  if (temp < 15) {
-    document.body.classList.add("cold-background");
-  } else if (temp > 30) {
-    document.body.classList.add("hot-background");
-  }
-}
 
 // Set the current date
 function setCurrentDate() {
